8caca97aad95b7b68c3f95c5b21db7fbf074922b
najin <105770758+najinp@users.noreply.github.com>
2022-10-14 15:36:41 +0900

    Add files via upload

--- cartier/js/main.js
+++ cartier/js/main.js
@@ -1,5 +1,16 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    document.querySelector('.tbbtn i').addEventListener('click', function () {
+        //this.classList.toggle('on');
+        document.querySelector('.TopBanner').classList.add('on');
+    });
+
+    document.querySelector('.mopen').addEventListener('click', function () {
+        document.querySelector('.header_bottom').classList.toggle('on');
+    });
+
+
+
     var artleft = new Swiper('.a_left', {
         loop: true,
         pagination: {
@@ -42,6 +53,42 @@ window.addEventListener('DOMContentLoaded', () => {
     //Main Swiper와 Thumbnail Swiper가 연동제어 서로를 제어	
     artleft.controller.control = artright;
     artright.controller.control = artleft;
+
+
+    // new Swiper('.classic_silide', {
+    //     slidesPerView: 1,
+    //     spaceBetween: 10,
+    // });
+
+    var swiper = new Swiper(".classic_slide", {
+        spaceBetween: 5,
+        slidesPerView: 1,
+        centeredSlides: false,
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true
+        },
+
+        breakpoints: {
+            // when window width is >= 320px
+
+            // when window width is >= 640px
+            768: {
+                slidesPerView: 6,
+                spaceBetween: 30
+            }
+        }
+    });
+
+
+
+
+
+
+
+
+
 
 
 })
